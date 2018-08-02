d7b8e8b5073d302fb631aa3b92b4530c6652a515
Tess <tguefen@gmail.com>
2018-08-01 21:36:47 -0700

    Yas 3

---
+++ js/batchlist.js
@@ -0,0 +1,68 @@
+function StoreLocator_Batchlist() {
+	var self = this;
+	Load_Additional_Fields( function( response) {
+		if ( response.success ) {
+			self.additional_fields = response.data;
+			self.additional_fields_length = self.additional_fields.length;
+			
+			MMBatchList.call( self, 'jsStoreLocator_Batchlist' );
+
+			self.SetDefaultSort( 'id', '-' );
+			self.Feature_Add_Enable('Add Location');
+			self.Feature_Edit_Enable('Edit Location(s)');
+			self.Feature_Delete_Enable('Delete Location(s)');
+			self.Feature_RowDoubleClick_Enable();
+			self.Feature_SearchBar_SetPlaceholderText( 'Search Locations...' );
+			self.Feature_Buttons_AddButton_Persistent( 'Settings', 'Settings', 'settings', self.Settings );
+			self.processingdialog = new ProcessingDialog();
+
+		}
+	});
+}
+
+DeriveFrom( MMBatchList, StoreLocator_Batchlist );
+
+StoreLocator_Batchlist.prototype.onLoad = StoreLocator_Load_Query;
+
+StoreLocator_Batchlist.prototype.onCreateRootColumnList = function() {
+		var self = this;
+		var i, i_len;
+		var columnlist = [];
+
+		columnlist.push(
+
+			new MMBatchList_Column_Name( 'ID', 'id', 'id')
+			.SetAdvancedSearchEnabled(false)
+			.SetDisplayInMenu(false)
+			.SetDisplayInList(false),
+			new MMBatchList_Column_CheckboxSlider('Active', 'active', 'active', function( item, checked, delegator ) {
+				StoreLocator_Batchlist.Update_Active( item, checked, delegator );
+			} ),
+			new MMBatchList_Column_Code( 'Code', 'code', 'code'),
+			new MMBatchList_Column_Name( 'Name', 'name', 'name'),
+			new MMBatchList_Column_Name( 'Address 1', 'addr1', 'addr1'),
+			new MMBatchList_Column_Name( 'Address 2', 'addr2', 'addr2'),
+			new MMBatchList_Column_Name( 'City', 'city', 'city'),
+			new MMBatchList_Column_Name( 'State', 'state', 'state'),
+			new MMBatchList_Column_Name( 'Zip/ Postal Code', 'zip', 'zip'),
+			new MMBatchList_Column_Name( 'Country', 'cntry', 'cntry'),
+			new MMBatchList_Column_Name( 'Latitude', 'lat', 'lat' ),
+			new MMBatchList_Column_Name( 'Longitude', 'lng', 'lng' )
+		);
+
+		for ( i = 0, i_len = self.additional_fields_length; i < i_len; i++ ) {
+			columnlist.push( new MMBatchList_Column_Text( 'Field: ' + self.additional_fields[ i ].name, 'AdditionalFields_' + self.additional_fields[ i ].code, 'AdditionalFields:' + self.additional_fields[ i ].code ).SetAdvancedSearchEnabled(false).SetSortByField( '' ) );
+		}
+
+	return columnlist;
+}
+
+StoreLocator_Batchlist.prototype.Settings = function() {
+	var self = this;
+	var dialog;
+
+	dialog			= new StoreLocatorSettingsDialog();
+	dialog.onsave	= function() { self.Refresh(); };
+
+	dialog.Show();
+}
---
+++ js/functions.js
@@ -0,0 +1,19 @@
+function Load_Additional_Fields( callback, delegator ) {
+	return AJAX_Call_Module(	callback,
+								'admin',
+								'TGStoreLocator',
+								'Load_Additional_Fields',
+								'',
+								delegator );
+}
+function StoreLocator_Load_Query( filter, sort, offset, count, callback, delegator ) {
+	return AJAX_Call_Module(	callback,
+								'admin',
+								'TGStoreLocator',
+								'StoreLocator_Load_Query',
+								'&Filter=' + EncodeArray( filter ) +
+								'&Sort=' + encodeURIComponent( sort ) +
+								'&Offset=' + encodeURIComponent( offset ) +
+								'&Count=' + encodeURIComponent( count ),
+								delegator );
+}
